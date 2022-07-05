f7f74f734338d4decf4812d0623c6d765f4830a0
Ian Wright <idwright90@gmail.com>
2022-07-05 14:34:44 -0400

    Store redirect URI as env variable

--- constants.rb
+++ constants.rb
@@ -2,5 +2,5 @@
 
 CLIENT_ID = ENV['CLIENT_ID']
 SECRET = ENV['SECRET']
-REDIRECT_URI = 'http://localhost:9292/'
+REDIRECT_URI = ENV['REDIRECT_URI']
 USER_AGENT = { 'User-Agent' => 'web:shiftit:v0.1.0 (by /u/heart_of_space)' }.freeze
